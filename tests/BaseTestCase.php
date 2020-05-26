b0f3ea680fc504b92bedeef997adef65885fcad5
zed <zedsmail@163.com>
2020-05-26 14:59:04 +0800

    fix: require version

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -14,7 +14,7 @@ use Orchestra\Testbench\TestCase;
 
 class BaseTestCase extends TestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
         /**
